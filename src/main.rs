487f37ed3bd66c49e3056a18ae6e7c2426f202b7
nogtk <taka.naoga@moneyforward.co.jp>
2021-06-06 16:30:41 +0900

    Extract API caller

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 mod util;
+mod client;
 
 use serde::Deserialize;
 use reqwest::Error;
-use reqwest::header::{HeaderMap, HeaderValue, USER_AGENT, AUTHORIZATION};
 use util::from_env;
 
 #[derive(Deserialize, Debug)]
@@ -10,26 +10,14 @@ struct PullRequest {
     html_url: String,
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Error> {
+fn main() -> Result<(), Error> {
     let owner = from_env("GITHUB_REPO_OWNER");
     let repository = from_env("GITHUB_REPO_NAME");
     let request_url = format!("https://api.github.com/repos/{owner}/{repo}/pulls",
                               owner = owner,
                               repo = repository);
-    let mut header_map = HeaderMap::new();
-    header_map.insert(
-        USER_AGENT, HeaderValue::from_static("foo")
-    );
-    let base = "Token ".to_string();
-    let token = from_env("GITHUB_TOKEN");
-    let authorization_header = format!("{}{}", base, token);
-    header_map.insert(
-        AUTHORIZATION, HeaderValue::from_str(&authorization_header).unwrap()
-    );
-    let client = reqwest::Client::new();
-    let res = client.get(&request_url).headers(header_map).send().await?;
-    let prs: Vec<PullRequest> = res.json().await?;
+    let response = client::get(&request_url).unwrap();
+    let prs: Vec<PullRequest> = response.json()?;
     println!("{:?}", prs);
     Ok(())
 }
