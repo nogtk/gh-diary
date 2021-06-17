6833d36824ea922a7dc6ebf03819bd55b8d1b6d6
nogtk <taka.naoga@moneyforward.co.jp>
2021-06-17 22:16:44 +0900

    Add test

--- src/util.rs
+++ src/util.rs
@@ -7,3 +7,18 @@ pub fn from_env(name: &str) -> String {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::util::from_env;
+
+    #[test]
+    fn success_from_env() {
+        std::env::set_var("FOR_FROM_ENV_TEST", "VALUE");
+
+        let result = from_env("FOR_FROM_ENV_TEST");
+        assert_eq!(result, "VALUE");
+
+        std::env::remove_var("FOR_FROM_ENV_TEST")
+    }
+}
